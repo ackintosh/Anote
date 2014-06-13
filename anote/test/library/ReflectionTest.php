522b983a23f3f58326a1268f707c44ff553867c7
Akihito Nakano <sora.akatsuki@gmail.com>
2014-06-13 23:45:09 +0900

    add test

---
+++ anote/test/library/ReflectionTest.php
@@ -0,0 +1,47 @@
+<?php
+namespace anote\library;
+/**
+ * Reflection Test
+ * @package Test
+ */
+
+class ReflectionTestSample
+{
+    /**
+     * ReflectionTestSample::func1()
+     */
+    public function func1() {}
+    public function func2() {}
+}
+
+class ReflectionTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function setUp()
+    {
+        $this->sample = new ReflectionTestSample;
+    }
+
+    /**
+     * @test
+     */
+    public function getMethodsReturnsArrayOfReflectionMethodObjects()
+    {
+        $expectNames = array('func1', 'func2');
+        $methods = Reflection::getMethods($this->sample);
+        foreach ($methods as $m) {
+            $this->assertInstanceOf('ReflectionMethod', $m);
+            $this->assertTrue(in_array($m->name, $expectNames));
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function getMethodCommentReturnsComment()
+    {
+        $expect = "/**\n     * ReflectionTestSample::func1()\n     */";
+        $comment = Reflection::getMethodComment($this->sample, 'func1');
+        $this->assertSame($expect, $comment);
+    }
+}
