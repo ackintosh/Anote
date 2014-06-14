b14e8baa664b1ac184358695d3395d62784eacfd
Akihito Nakano <sora.akatsuki@gmail.com>
2014-06-14 16:48:30 +0900

    fix travis

--- Anote/test/phpunit_bootstrap.php
+++ Anote/test/phpunit_bootstrap.php
@@ -6,7 +6,6 @@ define('ROOT', realpath(dirname(__FILE__) . '/../../') . '/');
 define('ANOTE_ROOT', ROOT . 'Anote/');
 define('WEB_ROOT', ROOT . 'webroot/');
 
-require_once 'PHPUnit/Autoload.php';
 require_once __DIR__ . '/TestHelper.php';
 require_once('../Library/Autoloader.php');
 
